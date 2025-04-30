b79c849de64d7a4749d18dca3bb392f09f44c766
Mateusz Lacki <matteo.lacki@gmail.com>
2025-04-30 18:27:18 +0200

    updated example

--- __dev/devel.py
+++ __dev/devel.py
@@ -4,6 +4,7 @@
 """
 
 import numpy as np
+import tqdm
 
 from prosit_timsTOF_2023_wrapper.main import Prosit2023TimsTofWrapper
 
@@ -13,24 +14,13 @@ collision_energies = np.array([30.0, 31.1])
 charges = np.array([1, 2])
 
 prosit = Prosit2023TimsTofWrapper()
-# prosit.annotations
-# prosit.get_fragment_intensity_annotations(10, 2)
-
-self = prosit
-
-from prosit_timsTOF_2023_wrapper.tokenization import tokenize_unimod_sequence
-
-import numba
-import numpy.typing as npt
-
-
-
-    prosit.iter_predict_intensities(
-        sequences=sequences,
-        amino_acid_cnts=amino_acid_cnts,
-        charges=charges,
-        collision_energies=collision_energies,
+with tqdm.tqdm(total=len(sequences)) as pbar:
+    fragment_intensity_lst = list(
+        prosit.iter_predict_intensities(
+            sequences=sequences,
+            amino_acid_cnts=amino_acid_cnts,
+            charges=charges,
+            collision_energies=collision_energies,
+            pbar=pbar,
+        )
     )
-
-
-
